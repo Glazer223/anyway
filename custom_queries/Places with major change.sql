710507abc61d53c2de7d6049ca3939828e50e968
Glazer223 <39100785+Glazer223@users.noreply.github.com>
2018-07-14 08:47:44 +0300

    Update Places with major change.sql

--- custom_queries/Places with major change.sql
+++ custom_queries/Places with major change.sql
@@ -1,5 +1,6 @@
 
-select  Q_2017.Round_long, Q_2017.Round_lat, Q_2016.Y_2016, Q_2017.Y_2017, abs((cast(Q_2017.Y_2017 as decimal)-Q_2016.Y_2016)) as diffrence, (cast(Q_2017.Y_2017 as decimal)/Q_2016.Y_2016) as Growth,
+select Q_2017.Round_lat,  Q_2017.Round_long, Q_2016.Y_2016, Q_2017.Y_2017, abs((cast(Q_2017.Y_2017 as decimal)-Q_2016.Y_2016)) as diffrence,
+ (cast(Q_2017.Y_2017 as decimal)/Q_2016.Y_2016) as Growth,osm_israel.osm_tags,osm_israel.osm_name,osm_israel.osm_primitive,
 case 
 WHEN  (cast(Q_2017.Y_2017 as decimal)/(Q_2016.Y_2016))>1 THEN 'Rise'
 WHEN (cast(Q_2017.Y_2017 as decimal)/(Q_2016.Y_2016))<1 THEN 'Decline'
@@ -26,10 +27,11 @@ from (
          where date_part ('year', created) = '2016'
          group by round_long, round_lat)  as Q_2016
          on (Q_2017.round_long =  Q_2016.round_long and Q_2016.round_lat =  Q_2017.round_lat))
+-- Geocoding
+join osm_israel
+on ST_covers (ST_setSRID(osm_israel."geometry",4326)::geography,st_makepoint((cast(Q_2017.round_long as float)), (cast(Q_2017.round_lat as float))))
 -- Filter for low results and no change
 where Q_2017.Y_2017 + Q_2016.Y_2016 > 25 
 and 
 ((cast(Q_2017.Y_2017 as decimal)/(Q_2016.Y_2016))> 1.3 or (cast(Q_2017.Y_2017 as decimal)/(Q_2016.Y_2016))< 0.7)
 order by diffrence desc
-
-;
